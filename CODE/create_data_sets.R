ddcc2b3911ea10bad326db5b278145f09e896eb5
Luiz Max de Carvalho <lmcarvalho@ufrj.br>
2016-10-24 19:07:00 +0100

    improving data collection

--- CODE/create_data_sets.R
+++ CODE/create_data_sets.R
@@ -1,24 +1,22 @@
 ### This script will retrieve the sequences from Genbank after we figured out which ones we want
-### Copyleft (or the one to blame): Carvalho, LMF (2015)
+### Copyleft (or the one to blame): Carvalho, LMF (2016)
+library(seqinr)
+dataBaseA <- data.frame(read.csv("../DATA/SEQUENCES/data_aquisition/serotype_A_SA_metadata_final.csv", header = TRUE))
+dataBaseO <- data.frame(read.csv("../DATA/SEQUENCES/data_aquisition/serotype_O_SA_metadata_final.csv", header = TRUE))  
+rawA <- read.fasta("../DATA/SEQUENCES/data_aquisition/SA_serotype_A_VP1_alignment.fasta")
+rawO <- read.fasta("../DATA/SEQUENCES/data_aquisition/SA_serotype_O_VP1_alignment.fasta")
 
-library(ape)
-dataBaseA <- data.frame(read.csv("../DATA/SEQUENCES/vp1_serotype_A.csv", header = TRUE))
-dataBaseO <- data.frame(read.csv("../DATA/SEQUENCES/vp1_serotype_O.csv", header = TRUE))  
-rawA <- read.GenBank(dataBaseA$Accession.number)
-rawO <- read.GenBank(dataBaseO$Accession.number)
-
-createSeqName <- function(database, i){
-  paste(database$Serotype[i], "_", i, "_", database$Accession.number[i],
-        "_", database$Location[i], "_", database$Year[i], sep = "")
+createSeqName <- function(database, i, serotype = "A"){
+  paste(serotype, "_", sprintf("%03d", i), "_", database$accession[i],
+        "_", database$country[i], "_", database$date[i], sep = "")
 }
 newNamesA <- sapply(1:nrow(dataBaseA), createSeqName, database = dataBaseA)
-newNamesO <- sapply(1:nrow(dataBaseO), createSeqName, database = dataBaseO)
+newNamesO <- sapply(1:nrow(dataBaseO), createSeqName, database = dataBaseO, serotype = "O")
 
-names(rawA) <- newNamesA
-names(rawO) <- newNamesO
+posA <- sapply(dataBaseA$accession, function(x) grep(x, names(rawA)))
+posO <- sapply(dataBaseO$accession, function(x) grep(x, names(rawO)))
+orderedA <- rawA[posA]
+orderedO <- rawO[posO] 
 
-rawA
-rawO
-
-write.dna(rawA, file = "../DATA/SEQUENCES/serotype_A_unaligned.fasta", format = "fasta" )
-write.dna(rawO, file = "../DATA/SEQUENCES/serotype_O_unaligned.fasta", format = "fasta" )
+write.fasta(orderedA, names = newNamesA, file = "../DATA/SEQUENCES/serotype_A_VP1_renamed.fasta")
+write.fasta(orderedO, names = newNamesO, file = "../DATA/SEQUENCES/serotype_O_VP1_renamed.fasta")
